e812c1b6fcdedd3b84380cd6f1a151458799d35a
Sumin0916 <gsmin2018@gmail.com>
2024-03-31 12:48:57 +0900

    #7 Flaks 폴더 구조 설계 & 초기 파일 생성

---
+++ pybo.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('./views')
+
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    import main_views
+    app.register_blueprint(main_views.bp)
+
+    return app
