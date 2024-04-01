e1480b6a67ed220eb696b45a464986df0438f996
Sumin0916 <gsmin2018@gmail.com>
2024-04-01 14:18:35 +0900

    Front-Back 연결

--- pybo.py
+++ pybo.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-
+    
     import main_views
     app.register_blueprint(main_views.bp)
 
--- views/main_views.py
+++ views/main_views.py
@@ -7,5 +7,13 @@ def hello_pybo():
     return 'Hello, Pybo!'
 
 @bp.route('/')
-def index():
+def main():
+    return render_template('main.html')
+
+@bp.route('/login_account')
+def login_account():
+    return render_template('login_account.html')
+
+@bp.route('/home')
+def home():
     return render_template('home.html')
